6ff51d86233081233f81d7428ce22b663a45004c
pvitkovsky <vitkovsky1337@gmail.com>
2018-08-28 14:30:46 +0300

    implemented some test methods

---
+++ ps2/src/com/util/randString/randString.java
@@ -0,0 +1,39 @@
+package com.util.randString;
+
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
+
+public class randString {
+
+	static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	static SecureRandom rnd = new SecureRandom();
+
+	public static String randomString(int len) {
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++)
+			sb.append(AB.charAt(rnd.nextInt(AB.length())));
+		return sb.toString();
+	}
+
+	/**
+	 * Generates a set of random strings
+	 * 
+	 * @param len
+	 *            length of strings to generate
+	 * @param size
+	 *            size of set to return
+	 * @return a set of random strings of size size
+	 */
+	public static Set<String> randomSet(int len, int size) {
+		Set<String> res = new HashSet<String>();
+		while (res.size() < size) {
+			res.add(randomString(len));
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(randomSet(5, 5));
+	}
+}
--- ps2/test/graph/GraphInstanceTest.java
+++ ps2/test/graph/GraphInstanceTest.java
@@ -6,8 +6,11 @@ package graph;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.Set;
 
 import org.junit.Test;
+
+import com.util.randString.randString;
 
 /**
  * Tests for instance methods of Graph.
@@ -19,13 +22,19 @@ import org.junit.Test;
  */
 public abstract class GraphInstanceTest {
     
-    // Testing strategy
+	//Violating instructions for code reusability; 
+    private static final int BUFFERSIZE = 5;
+    private static final int BUFFERNAMELENGTH = 6;
+    private static final Set<String> EXTRASTRINGS = randString.randomSet(BUFFERSIZE, BUFFERNAMELENGTH);
+	
+	// Testing strategy
     //   Fail-fast on null arguments is tested with each graph method;
 	//   Partition our methods as following:
 	//   Add: Element e exists; not exists;
+    //   Set: Illegal argument first/second/third; Legal arguments; 
 	//   Remove: Element e from: empty; containing e; non-containing e;
-	//   Set: Illegal argument first/second/third; Legal arguments; 
-	//   Source: target illegal; target legal and has 0/1/n sources;
+	//   Vertices: Add/remove a set of strings, checking only unique strings count;
+    //   Source: target illegal; target legal and has 0/1/n sources;
     //   Target: source illegal; source legal and has 0/1/n targets;
     
     /**
@@ -51,19 +60,76 @@ public abstract class GraphInstanceTest {
     public void testNullFailFast(){}
     
     @Test
-    public void testAddNew(){}
+    public void testAddNew(){
+    	Graph<String> graph = emptyInstance();
+    	assertEquals("Adding new vertex modifies the graph",
+                true, graph.add("Test"));
+    }
     
     @Test
-    public void testAddExisting(){}
+    public void testAddExisting(){
+    	Graph<String> graph = emptyInstance();
+    	graph.add("Test");
+    	assertEquals("Every vertex is unique",
+                false, graph.add("Test"));
+    }
+    
+    @Test 
+    public void testVerticesSize() {
+    	Graph<String> graph = emptyInstance();
+    	assertEquals("Zero size in emptyInstance",
+                0, graph.vertices().size());
+    	for(String str : EXTRASTRINGS) {
+    		graph.add(str);
+    	}
+    	assertEquals("One size after adding one vertex",
+    			EXTRASTRINGS.size(), graph.vertices().size());
+    	graph.add(EXTRASTRINGS.iterator().next());
+    	assertEquals("Adding exising vertice doesn't change graph size",
+    			EXTRASTRINGS.size(), graph.vertices().size());
+    	for(String str : EXTRASTRINGS) {
+    		graph.remove(str);
+    	}
+    	assertEquals("Zero size after removing one vertex",
+                0, graph.vertices().size());
+    }
+
+    @Test 
+    public void testVerticesContains() {
+    	Graph<String> graph = emptyInstance();
+    	for(String str : EXTRASTRINGS) {
+    		graph.add(str);
+    	}
+    	String test = EXTRASTRINGS.iterator().next();
+    	assertEquals("Vertices contain what was added",
+                true, graph.vertices().contains(test));
+    	graph.remove(test);
+    	assertEquals("Vertices do not contain what was removed",
+                false, graph.vertices().contains(test));
+    }
     
     @Test
-    public void testRemoveFromEmpty(){}
+    public void testRemoveFromEmpty(){
+    	Graph<String> graph = emptyInstance();
+    	assertEquals("Removing something from empty graph does no modify it",
+                false, graph.remove("Test"));
+    }
     
     @Test
-    public void testRemoveFromContaining(){}
-    
+    public void testRemoveFromContaining(){
+    	Graph<String> graph = emptyInstance();
+    	graph.add("Test");
+    	assertEquals("Removing what exist does modify the graph",
+                true, graph.remove("Test"));
+    }
+
     @Test
-    public void testRemoveFromNotContaining(){}
+    public void testRemoveFromNotContaining(){
+    	Graph<String> graph = emptyInstance();
+    	graph.add("Test");
+    	assertEquals("Removing what does not exist does not modify the graph",
+                false, graph.remove("AnotherTest"));
+    }
     
     @Test
     public void testSetIllegalSource(){}
