64df3d874a4f1cfec948bd870dcac234150e6685
pvitkovsky <vitkovsky1337@gmail.com>
2018-08-28 15:03:32 +0300

    debugging in generator

--- ps2/test/graph/VerticeSetGenerator.java
+++ ps2/test/graph/VerticeSetGenerator.java
@@ -2,29 +2,31 @@ package graph;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.util.randString.randString;
 
 /**
- * Keeps a set of random strings and a unique string;
- * Unique string is guaranteed not to be present in the set
+ * Keeps a set of random strings and a unique string; Unique string is
+ * guaranteed not to be present in the set
  */
 public class VerticeSetGenerator {
 	static final int BUFFERSIZE = 5;
 	static final int BUFFERNAMELENGTH = 6;
-	static final Set<String> EXTRASTRINGS = randString.randomSet(BUFFERSIZE, BUFFERNAMELENGTH);
-	static final String EXTRAFIRST = EXTRASTRINGS.iterator().next();
+	static final Set<String> SET = randString.randomSet(BUFFERSIZE, BUFFERNAMELENGTH);
+	static final String UNIQUE = SET.iterator().next();
 
 	public static Set<String> getExtraStrings() {
-		assertTrue(EXTRASTRINGS.remove(EXTRAFIRST));
-		assertTrue(EXTRASTRINGS.size() > 0);
-		return EXTRASTRINGS;
+		Set<String> res = new HashSet<String>(SET);
+		res.remove(UNIQUE);
+		assertTrue(SET.size() - res.size() == 1);
+		return res;
 	}
 
 	public static String getUniqueString() {
-		assertTrue(EXTRAFIRST != null);
-		return EXTRAFIRST;
+		assertTrue(UNIQUE != null);
+		return UNIQUE;
 	}
 
 }
