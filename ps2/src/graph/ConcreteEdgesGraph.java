0e2833e527525be55efdaa792897c0ab05fcedf0
pvitkovsky <vitkovsky1337@gmail.com>
2018-09-12 14:41:11 +0300

    pseudocode for set

--- ps2/src/graph/ConcreteEdgesGraph.java
+++ ps2/src/graph/ConcreteEdgesGraph.java
@@ -30,16 +30,37 @@ public class ConcreteEdgesGraph implements Graph<String> {
     
     // TODO checkRep
     
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    @Override public boolean add(String name) {
+    	if (exists(name)) return false;
+    	return vertices.add(name);
+    }
+    
+    private boolean exists(String name) {
+    	return vertices.contains(name);
     }
     
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+    	if (!exists(source)) throw new IllegalArgumentException("Source not found");
+    	if (!exists(target)) throw new IllegalArgumentException("Target not found");
+    	// if can't find edge with this source and target;
+    		// if weigth is 0; 
+    		// else create edge; 
+    		// return 0;
+    	// else remember edge's oldweight
+    		// if weight is 0 set edge's weigth to 0 // or kill edge??
+    		// else set edge's weight to weight
+    		// return oldweight;
+    	if(weight != 0) {
+       		//edges.add(new Edge(source, target, weight)); 
+    	} else {
+    		//edges.remove(find this edge)
+    	}
+  
+    	throw new RuntimeException("not implemented");
     }
     
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+    @Override public boolean remove(String name) {
+        return vertices.remove(name);
     }
     
     @Override public Set<String> vertices() {
@@ -69,6 +90,9 @@ public class ConcreteEdgesGraph implements Graph<String> {
 class Edge {
     
     // TODO fields
+	final int weight;
+	final String source;
+	final String target;
     
     // Abstraction function:
     //   TODO
@@ -77,7 +101,25 @@ class Edge {
     // Safety from rep exposure:
     //   TODO
     
-    // TODO constructor
+    public String getSource() {
+		return source;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	// TODO constructor
+	Edge(String source, String target,int weight){
+		this.source = source;
+		this.target = target;
+		this.weight = weight;
+		
+	}
+
+	int getWeight() {
+		return weight;
+	}
     
     // TODO checkRep
     
